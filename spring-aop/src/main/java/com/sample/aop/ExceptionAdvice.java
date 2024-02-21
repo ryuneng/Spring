432f7cac127cd93d23ce1fa237295f05683429b3
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-02-21 17:51:46 +0900

    Spring Day2
    1. Setter Injection / Constructor Injection (setter 의존성 주입 / 생성자 의존성 주입)
    2. Bean Scope(Singleton)
    3. 같은 타입의 여러개 Dao클래스가 있을 때 한정자 @Primary
    4. Spring Container
    5. AOP

---
+++ spring-aop/src/main/java/com/sample/aop/ExceptionAdvice.java
@@ -0,0 +1,15 @@
+package com.sample.aop; // 20240221 Day2
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ExceptionAdvice {
+
+	@AfterThrowing(pointcut = "within(com.sample.service.*)", throwing = "ex")
+	public void handleException(Exception ex) {
+		System.out.println("예외가 발생했습니다. 오류 메세지: " + ex.getMessage());
+	}
+}
---
+++ spring-aop/src/main/java/com/sample/vo/User.java
@@ -0,0 +1,42 @@
+package com.sample.vo; // 20240221 Day2
+
+public class User {
+
+	private String id;
+	private String password;
+	private String name;
+	
+	public User() {}
+
+	public User(String id, String password, String name) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
